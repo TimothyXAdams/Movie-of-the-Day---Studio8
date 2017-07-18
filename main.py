ab6b81e575b6308739a0563b3f5037c5287d134d
Timothy Adams <TimothyXAdams@gmail.com>
2017-07-17 20:31:00 -0700

    connected User and Movie in a one-to-many relationship

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@loc
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
+app.secret_key = 'yyyy6666'
+
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    id       = db.Column(db.Integer, primary_key=True)
+    email    = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    
+    movies   = db.relationship('Movie', backref='owner')
+
     def __init__(self, email, password):
         self.email = email
         self.password = password
@@ -26,10 +29,12 @@ class Movie(db.Model):
     name = db.Column(db.String(120))
     watched = db.Column(db.Boolean)
     rating = db.Column(db.String(20))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
         self.watched = False
+        self.owner = owner
 
     def __repr__(self):
         return '<Movie %r>' % self.name
