a599df10da413d04665c54741d2f470a732ce707
Timothy Adams <TimothyXAdams@gmail.com>
2017-07-17 20:50:03 -0700

    moved app and database config to app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+#The app and database configuration
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://flicklist:MyNewPass@localhost:8889/flicklist'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db = SQLAlchemy(app)
+app.secret_key = 'yyyy6666'
